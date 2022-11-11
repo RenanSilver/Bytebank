57c743350a68dd6a030f84626fca5af69473fc40
RenanSilver <101308960+RenanSilver@users.noreply.github.com>
2022-11-11 15:14:41 -0300

    entendendo como funciona uma variavel no Kotlin. Double/Int.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,12 @@
 fun main() {
     println("Hello World ")
-    val titular = "Renan"
+    val titular: String  = "Renan"
+    val numeroConta: Int = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
 
     println("Titular: $titular")
+    println("Numero da conta: $numeroConta")
+    println("Saldo da conta: $saldo")
 }
