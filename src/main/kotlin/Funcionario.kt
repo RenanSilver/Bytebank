3090ff6d1f482d4802b0459f9d4d6789623ea019
RenanSilver <101308960+RenanSilver@users.noreply.github.com>
2022-11-14 12:46:28 -0300

    Problemas ao usar modo procedural em um TIPO de objeto.

--- src/main/kotlin/Funcionario.kt
+++ src/main/kotlin/Funcionario.kt
@@ -1,6 +1,20 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double
+    val salario: Double,
+    val tipo: Int
 ) {
+    fun bonificacao(): Double {
+        when (tipo) {
+            0 -> {
+                return salario * 0.1
+            }
+            1 -> {
+                return salario * 0.2
+            }
+            else -> {
+                return salario * 0.3
+            }
+        }
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,16 @@
 fun main() {
     println("Bytebank")
 
+    val renanFuncionario = Funcionario(
+        nome = "Renan",
+        cpf = "42632896843",
+        salario = 1890.0
+    )
+
+    println("nome: ${renanFuncionario.nome}")
+    println("cpf: ${renanFuncionario.cpf}")
+    println("salario: ${renanFuncionario.salario}")
+    println("bonificacao: ${renanFuncionario.bonificacao()}")
 }
 
 
