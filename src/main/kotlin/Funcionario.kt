ef00373530f8f1e37cf514b7b088ded3cfdeb2f0
RenanSilver <101308960+RenanSilver@users.noreply.github.com>
2022-11-14 14:02:26 -0300

    criando a class Gerente.kt e entendendo as exclusividades de classes

--- src/main/kotlin/Funcionario.kt
+++ src/main/kotlin/Funcionario.kt
@@ -1,20 +1,9 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double,
-    val tipo: Int
+    val salario: Double
 ) {
     fun bonificacao(): Double {
-        when (tipo) {
-            0 -> {
-                return salario * 0.1
-            }
-            1 -> {
-                return salario * 0.2
-            }
-            else -> {
-                return salario * 0.3
-            }
-        }
+        return salario * 0.1
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,11 +6,32 @@ fun main() {
         cpf = "42632896843",
         salario = 1890.0
     )
+    val karinaGerente = Gerente(
+        nome = "Karina",
+        cpf = "605.987.362.98",
+        salario = 2800.0,
+        senha = 12345
+    )
+
 
     println("nome: ${renanFuncionario.nome}")
     println("cpf: ${renanFuncionario.cpf}")
     println("salario: ${renanFuncionario.salario}")
     println("bonificacao: ${renanFuncionario.bonificacao()}")
+    println("-----------------------------------------------")
+
+    println("nome: ${karinaGerente.nome}")
+    println("cpf: ${karinaGerente.cpf}")
+    println("salario: ${karinaGerente.salario}")
+    println("bonificacao: ${karinaGerente.bonificacao()}")
+
+
+
+    if(karinaGerente.autentica(22222)){
+        println("Sucesso")
+    } else {
+        println("Senha incorreta")
+    }
 }
 
 
