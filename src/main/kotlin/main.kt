848a44efd46188cb15ac3cbfa33a75f75c02b042
RenanSilver <101308960+RenanSilver@users.noreply.github.com>
2022-11-20 15:30:25 -0300

    Usando polimorfismo na classe Conta.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,72 +1,43 @@
 fun main() {
     println("Bytebank")
 
-    val renanFuncionario = Funcionario(
-        nome = "Renan",
-        cpf = "42632896843",
-        salario = 1890.0
+    val contaCorrente = ContaCorrente(
+        titular = "Renan",
+        numero = 555
     )
-    val karinaGerente = Gerente(
-        nome = "Karina",
-        cpf = "605.987.362.98",
-        salario = 2800.0,
-        senha = 12345
-    )
-    val franciscaoDiretor = Diretor(
-        nome = "Francisco",
-        cpf = "785.968.325-96",
-        salario = 4000000.0,
-        senha = 987654,
-        plr = 50000.0
+    val contaPoupanca = ContaPoupanca(
+        titular = "Karina",
+        numero = 444
     )
 
-    val maria = Analista(
-        nome = "Maria",
-        cpf = "555.999.666.88",
-        salario = 5000.0
-    )
+    contaCorrente.deposita(1000.0)
+    contaPoupanca.deposita(1000.0)
+
+    println("saldo corrente: ${contaCorrente.saldo}")
+    println("saldo poupanca: ${contaPoupanca.saldo}")
+
+    println("----------------------------------------------")
+
+    contaCorrente.saca(100.0)
+    contaPoupanca.saca(100.0)
+
+    println("----------------------------------------------")
+
+    println("saldo apos saque corrente: ${contaCorrente.saldo}")
+    println("saldo apos saque poupanca: ${contaPoupanca.saldo}")
+
+
+    println("----------------------------------------------")
+    contaPoupanca.transfere(100.0, contaCorrente)
+
+    println("saldo apos transferencia corrente<: ${contaCorrente.saldo}")
+    println("saldo apos transferencia poupanca>: ${contaPoupanca.saldo}")
 
 
 
-    println("nome: ${renanFuncionario.nome}")
-    println("cpf: ${renanFuncionario.cpf}")
-    println("salario: ${renanFuncionario.salario}")
-    println("bonificacao: ${renanFuncionario.bonificacao}")
-    println("-----------------------------------------------")
-
-    println("nome: ${karinaGerente.nome}")
-    println("cpf: ${karinaGerente.cpf}")
-    println("salario: ${karinaGerente.salario}")
-    println("bonificacao: ${karinaGerente.bonificacao}")
-    println("-----------------------------------------------")
-
-    println("nome: ${franciscaoDiretor.nome}")
-    println("cpf: ${franciscaoDiretor.cpf}")
-    println("salario: ${franciscaoDiretor.salario}")
-    println("bonificacao: ${franciscaoDiretor.bonificacao}")
-    println("participacao nos lucros: ${franciscaoDiretor.plr}")
-    println("---------------*-*-*-**-----------*-*-*--*---------*-*-*-*-")
-
-    println("nome: ${maria.nome}")
-    println("cpf: ${maria.cpf}")
-    println("salario: ${maria.salario}")
-    println("bonificacao: ${maria.bonificacao}")
-    println("-----------------------------------------------")
-
-    val calculadora = CalculadoraBonificacao()
-    calculadora.registra(karinaGerente)
-    calculadora.registra(renanFuncionario)
-    calculadora.registra(franciscaoDiretor)
-    calculadora.registra(maria)
-
-    println("Total das bonificacoes: ${calculadora.total}")
-
-
-    if (karinaGerente.autentica(22222)) {
-        println("Sucesso")
-    } else {
-        println("Senha incorreta")
-    }
 }
+
+
+
 
 
---
+++ src/main/kotlin/testaFuncionario.kt
@@ -0,0 +1,59 @@
+class testaFuncionario {
+    fun testaFuncionario() {
+        val renanFuncionario = Funcionario(
+            nome = "Renan", cpf = "42632896843", salario = 1890.0
+        )
+        val karinaGerente = Gerente(
+            nome = "Karina", cpf = "605.987.362.98", salario = 2800.0, senha = 12345
+        )
+        val franciscaoDiretor = Diretor(
+            nome = "Francisco", cpf = "785.968.325-96", salario = 4000000.0, senha = 987654, plr = 50000.0
+        )
+
+        val maria = Analista(
+            nome = "Maria", cpf = "555.999.666.88", salario = 5000.0
+        )
+
+
+
+        println("nome: ${renanFuncionario.nome}")
+        println("cpf: ${renanFuncionario.cpf}")
+        println("salario: ${renanFuncionario.salario}")
+        println("bonificacao: ${renanFuncionario.bonificacao}")
+        println("-----------------------------------------------")
+
+        println("nome: ${karinaGerente.nome}")
+        println("cpf: ${karinaGerente.cpf}")
+        println("salario: ${karinaGerente.salario}")
+        println("bonificacao: ${karinaGerente.bonificacao}")
+        println("-----------------------------------------------")
+
+        println("nome: ${franciscaoDiretor.nome}")
+        println("cpf: ${franciscaoDiretor.cpf}")
+        println("salario: ${franciscaoDiretor.salario}")
+        println("bonificacao: ${franciscaoDiretor.bonificacao}")
+        println("participacao nos lucros: ${franciscaoDiretor.plr}")
+        println("---------------*-*-*-**-----------*-*-*--*---------*-*-*-*-")
+
+        println("nome: ${maria.nome}")
+        println("cpf: ${maria.cpf}")
+        println("salario: ${maria.salario}")
+        println("bonificacao: ${maria.bonificacao}")
+        println("-----------------------------------------------")
+
+        val calculadora = CalculadoraBonificacao()
+        calculadora.registra(karinaGerente)
+        calculadora.registra(renanFuncionario)
+        calculadora.registra(franciscaoDiretor)
+        calculadora.registra(maria)
+
+        println("Total das bonificacoes: ${calculadora.total}")
+
+
+        if (karinaGerente.autentica(22222)) {
+            println("Sucesso")
+        } else {
+            println("Senha incorreta")
+        }
+    }
+}
