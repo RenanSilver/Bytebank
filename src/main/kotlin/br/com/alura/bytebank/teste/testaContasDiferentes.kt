54b7d3b8acfda77cbe7f6aa08769b33be77c9d93
RenanSilver <101308960+RenanSilver@users.noreply.github.com>
2022-12-07 00:30:13 -0300

    Adicionando a classe Endereco.kt, e compondo ela a classe Cliente.kt

--- src/main/kotlin/br/com/alura/bytebank/teste/testaContasDiferentes.kt
+++ src/main/kotlin/br/com/alura/bytebank/teste/testaContasDiferentes.kt
@@ -1,8 +1,19 @@
 import br.com.alura.bytebank.modelo.*
 
 fun testaContasDiferentes() {
-    val renan = Cliente(nome = "Renan", cpf = "54654654", senha = 222222)
-    val karina = Cliente(nome = "Karina", cpf = "123123", senha = 33333)
+    val renan = Cliente(
+        nome = "Renan",
+        cpf = "54654654",
+        senha = 222222,
+        endereco = Endereco(
+            logradouro = "Rua Tijucas do Sul"
+        )
+    )
+    val karina = Cliente(
+        nome = "Karina",
+        cpf = "123123",
+        senha = 33333
+    )
 
     val contaCorrente = ContaCorrente(
         titular = renan,
@@ -16,23 +27,24 @@ fun testaContasDiferentes() {
     contaCorrente.deposita(1000.0)
     contaPoupanca.deposita(1000.0)
 
-    println("saldo corrente: ${contaCorrente.saldo}")
-    println("saldo poupanca: ${contaPoupanca.saldo}")
+    println("saldo corrente: ${contaCorrente.titular.nome}")
+    println("saldo poupanca: ${contaPoupanca.titular.cpf}")
+    println("endereco do Titular: ${contaCorrente.titular.endereco.logradouro}")
 
     println("----------------------------------------------")
 
-    contaCorrente.saca(100.0)
-    contaPoupanca.saca(100.0)
-
-    println("----------------------------------------------")
-
-    println("saldo apos saque corrente: ${contaCorrente.saldo}")
-    println("saldo apos saque poupanca: ${contaPoupanca.saldo}")
-
-
-    println("----------------------------------------------")
-    contaPoupanca.transfere(100.0, contaCorrente)
-
-    println("saldo apos transferencia corrente<: ${contaCorrente.saldo}")
-    println("saldo apos transferencia poupanca>: ${contaPoupanca.saldo}")
+//    contaCorrente.saca(100.0)
+//    contaPoupanca.saca(100.0)
+//
+//    println("----------------------------------------------")
+//
+//    println("saldo apos saque corrente: ${contaCorrente.saldo}")
+//    println("saldo apos saque poupanca: ${contaPoupanca.saldo}")
+//
+//
+//    println("----------------------------------------------")
+//    contaPoupanca.transfere(100.0, contaCorrente)
+//
+//    println("saldo apos transferencia corrente<: ${contaCorrente.saldo}")
+//    println("saldo apos transferencia poupanca>: ${contaPoupanca.saldo}")
 }
